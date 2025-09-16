655559e227cea0a88253fb9449d170ce0d4e57ff
itsmedongyeon <q68640611@gmail.com>
2025-09-16 19:27:31 +0900

    1652.py 업데이트

--- 1652.py
+++ 1652.py
@@ -1,1 +1,39 @@
-.
+condo=list()
+n=int(input())
+for i in range(n):
+    room=input()
+    condo.append(room)
+rowcnt=0
+colcnt=0
+for i in range(n):
+    cnt=0
+    for j in range(n):
+        if condo[i][j]==".":
+            cnt+=1
+        if condo[i][j]=="X":
+            if cnt>=2:
+                rowcnt+=1
+                cnt=0
+            else:
+                cnt=0
+        if j==n-1:
+            if cnt>=2:
+                rowcnt+=1
+
+
+for i in range(n):
+    cnt=0
+    for j in range(n):
+        if condo[j][i]==".":
+            cnt+=1
+        if condo[j][i]=="X":
+            if cnt>=2:
+                colcnt+=1
+                cnt=0
+            else:
+                cnt=0
+        if j==n-1:
+            if cnt>=2:
+                colcnt+=1
+
+print(rowcnt,colcnt)
